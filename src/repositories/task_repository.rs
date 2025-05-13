d0f4101beaf8b12840977653f4f909cb0afd5ec2
martuafernando <fernandosibarani45@gmail.com>
2025-05-13 11:21:01 +0700

    feat: add dynamic id implementation

--- src/repositories/task_repository.rs
+++ src/repositories/task_repository.rs
@@ -1,7 +1,6 @@
 use crate::models::task::Task;
 use crate::models::task_status::TaskStatus;
 use crate::storage::file_storage::Storage;
-use std::collections::HashMap;
 use std::error::Error;
 pub trait TaskRepository {
     fn create(&mut self, name: &String, status: TaskStatus) -> Result<i8, Box<dyn Error>>;
@@ -16,39 +15,44 @@ impl<S: Storage> TaskRepositoryImpl<S> {
     pub fn new(storage: S) -> Self {
         Self { storage }
     }
+
+    fn get_new_id(&self, tasks: &[Task]) -> i8 {
+        tasks
+            .iter()
+            .max_by_key(|taks| taks.id)
+            .map(|task| task.id + 1)
+            .unwrap_or(1)
+    }
 }
 
 impl<S: Storage> TaskRepository for TaskRepositoryImpl<S> {
     fn create(&mut self, name: &String, status: TaskStatus) -> Result<i8, Box<dyn Error>> {
+        let mut data = self.storage.load()?;
+        let new_id = self.get_new_id(&data);
+
         let task = Task {
-            id: 1,
+            id: new_id,
             name: name.clone(),
             status: status,
         };
-        
-        
+
         let task_id = task.id;
 
-        let mut data = self.storage.load()?;
         data.push(task);
         self.storage.save(&data)?;
-        
+
         Ok(task_id)
     }
 
     fn get(&self, status: Option<TaskStatus>) -> Result<Vec<Task>, Box<dyn Error>> {
         let tasks = self.storage.load()?;
-    
+
         match status {
-            None => {
-                Ok(tasks)
-            },
-            Some(status_filter) => {
-                Ok(tasks
-                    .into_iter()
-                    .filter(|task| task.status == status_filter)
-                    .collect())
-            },
+            None => Ok(tasks),
+            Some(status_filter) => Ok(tasks
+                .into_iter()
+                .filter(|task| task.status == status_filter)
+                .collect()),
         }
     }
 }
